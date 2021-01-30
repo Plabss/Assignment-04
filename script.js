25a6c53804b44e8579a405835b704c62b37322b7
plabon chowdhury <plabonchowdhury9@gmail.comgit config --global user.name Plabssgit config --global user.email plabonchowdhury9@gmail.com>
2021-01-30 19:07:41 +0600

    3rd commit

--- script.js
+++ script.js
@@ -1,157 +1,88 @@
-// first class + sign
+function increment(numberOf) {
+    var number = document.getElementById(numberOf).value;
+    number = parseInt(number);
+    number = number + 1;
+    document.getElementById(numberOf).value = number;
 
- const increaseFirstClass=document.getElementById("increaseFirstClass");
- increaseFirstClass.addEventListener("click",function(){
-     var numberOfFirstClass=document.getElementById("numberOfFirstClass").value ;
-     numberOfFirstClass=parseInt(numberOfFirstClass);
-     numberOfFirstClass=numberOfFirstClass+1;
-     document.getElementById("numberOfFirstClass").value=numberOfFirstClass;
-     
-     const subtotalCost=subtotalVatTotal();
-     document.getElementById("subtotal").innerText=subtotalCost[0];
-
-
-     const vat=subtotalVatTotal();
-     document.getElementById("vat").innerText=vat[1];
-
-
-     const total=subtotalVatTotal();
-     document.getElementById("total").innerText=total[2];
-
- })
-
-
-
-
- // first class - sign
-
- const decreaseFirstClass=document.getElementById("decreaseFirstClass");
- decreaseFirstClass.addEventListener("click",function(){
-     var numberOfFirstClass=document.getElementById("numberOfFirstClass").value ;
-     numberOfFirstClass=parseInt(numberOfFirstClass);
-     if(numberOfFirstClass>0){
-        numberOfFirstClass=numberOfFirstClass-1;
-     }
-     document.getElementById("numberOfFirstClass").value=numberOfFirstClass;
-     
-     const subtotalCost=subtotalVatTotal();
-     document.getElementById("subtotal").innerText=subtotalCost[0];
-
-
-     const vat=subtotalVatTotal();
-     document.getElementById("vat").innerText=vat[1];
-
-
-     const total=subtotalVatTotal();
-     document.getElementById("total").innerText=total[2];
-
- })
-
-
- // Economy + sign
-
- const increaseEconomy=document.getElementById("increaseEconomy");
- increaseEconomy.addEventListener("click",function(){
-     var numberOfEconomy=document.getElementById("numberOfEconomy").value ;
-     numberOfEconomy=parseInt(numberOfEconomy);
-     numberOfEconomy=numberOfEconomy+1;
-     document.getElementById("numberOfEconomy").value=numberOfEconomy;
-    
-     const subtotalCost=subtotalVatTotal();
-     document.getElementById("subtotal").innerText=subtotalCost[0];
-
-
-     const vat=subtotalVatTotal();
-     document.getElementById("vat").innerText=vat[1];
-
-
-     const total=subtotalVatTotal();
-     document.getElementById("total").innerText=total[2];
-
- })
-
-
-
-
- // Economy - sign
-
- const decreaseEconomy=document.getElementById("decreaseEconomy");
- decreaseEconomy.addEventListener("click",function(){
-     var numberOfEconomy=document.getElementById("numberOfEconomy").value ;
-     numberOfEconomy=parseInt(numberOfEconomy);
-     if(numberOfEconomy>0){
-        numberOfEconomy=numberOfEconomy-1;
-     }
-     document.getElementById("numberOfEconomy").value=numberOfEconomy;
-     
-     const subtotalCost=subtotalVatTotal();
-     document.getElementById("subtotal").innerText=subtotalCost[0];
-
-
-     const vat=subtotalVatTotal();
-     document.getElementById("vat").innerText=vat[1];
-
-
-     const total=subtotalVatTotal();
-     document.getElementById("total").innerText=total[2];
-
- })
-
-
- // subtotal,vat,total calculation
-
-
-function subtotalVatTotal(){
-    const numberOfFirstClass=parseInt(document.getElementById("numberOfFirstClass").value);
-    const numberOfEconomy=parseInt(document.getElementById("numberOfEconomy").value);
-    return [((numberOfFirstClass*150) + (numberOfEconomy*100)),(((numberOfFirstClass*150) + (numberOfEconomy*100))*0.1),(((numberOfFirstClass*150) + (numberOfEconomy*100))+(((numberOfFirstClass*150) + (numberOfEconomy*100))*0.1))];
-    // return subtotal*0.1;
-    // return subtotal+vat;
+    costCalculation();
 }
 
 
+function decrement(numberOf) {
+    var number = document.getElementById(numberOf).value;
+    number = parseInt(number);
+    if (number > 0) {
+        number = number - 1;
+    }
+    document.getElementById(numberOf).value = number;
 
+    costCalculation();
+}
 
+// subtotal,vat,total calculation
 
-bookBtn=document.getElementById("bookBtn");
-bookBtn.addEventListener("click",function(){
-    const firstPage=document.getElementById("firstPage");
-    firstPage.style.display="none";
-    const secondPage=document.getElementById("secondPage");
-    secondPage.style.display="block"
-    const body=document.getElementById("body");
-    body.style.background="none";
+function costCalculation() {
+    const subtotalCost = subtotalVatTotal();
+    document.getElementById("subtotal").innerText = subtotalCost[0];
 
-    var numberOfFirstClassTicket=document.getElementById("numberOfFirstClass").value;
-    document.getElementById("firstclassTicket").innerText=numberOfFirstClassTicket + " "
+    const vat = subtotalVatTotal();
+    document.getElementById("vat").innerText = vat[1];
 
+    const total = subtotalVatTotal();
+    document.getElementById("total").innerText = total[2];
+}
 
-    var numberOfEconomyTicket=document.getElementById("numberOfEconomy").value;
-    document.getElementById("economyTicket").innerText=numberOfEconomyTicket + " "
-    
+function subtotalVatTotal() {
+    const numberOfFirstClass = parseInt(document.getElementById("numberOfFirstClass").value);
+    const numberOfEconomy = parseInt(document.getElementById("numberOfEconomy").value);
+    var subtotalValue = ((numberOfFirstClass * 150) + (numberOfEconomy * 100));
+    var vatValue = subtotalValue * 0.1;
+    var totalValue = subtotalValue + vatValue;
+    return [subtotalValue, vatValue, totalValue];
+}
+
+// first class + sign
+
+const increaseFirstClass = document.getElementById("increaseFirstClass");
+increaseFirstClass.addEventListener("click", function () {
+    increment("numberOfFirstClass");
 })
 
+// Economy + sign
 
+const increaseEconomy = document.getElementById("increaseEconomy");
+increaseEconomy.addEventListener("click", function () {
+    increment("numberOfEconomy");
+});
 
+// first class - sign
 
+const decreaseFirstClass = document.getElementById("decreaseFirstClass");
+decreaseFirstClass.addEventListener("click", function () {
+    decrement("numberOfFirstClass");
+})
 
+// Economy - sign
 
+const decreaseEconomy = document.getElementById("decreaseEconomy");
+decreaseEconomy.addEventListener("click", function () {
+    decrement("numberOfEconomy");
+})
 
+// For "Book Now" Button.
 
+bookBtn = document.getElementById("bookBtn");
+bookBtn.addEventListener("click", function () {
+    const firstPage = document.getElementById("firstPage");
+    firstPage.style.display = "none";
+    const secondPage = document.getElementById("secondPage");
+    secondPage.style.display = "block"
+    const body = document.getElementById("body");
+    body.style.background = "none";
 
+    var numberOfFirstClassTicket = document.getElementById("numberOfFirstClass").value;
+    document.getElementById("firstclassTicket").innerText = numberOfFirstClassTicket + " "
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
+    var numberOfEconomyTicket = document.getElementById("numberOfEconomy").value;
+    document.getElementById("economyTicket").innerText = numberOfEconomyTicket + " "
+})
